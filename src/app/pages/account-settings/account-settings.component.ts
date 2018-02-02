7612672803a23246889da9af1827596ae4efcc0c
ctorres <ctorres014@gmail.com>
2018-02-01 23:15:39 -0300

    Se finalizo con accountsettings para configurar un tema

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { DOCUMENT } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
+import { SettingsService } from './../../services/settings.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -8,32 +8,23 @@ import { DOCUMENT } from '@angular/platform-browser';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  constructor(@Inject(DOCUMENT) private _document) { }
+  constructor(private _setting: SettingsService) { }
 
   ngOnInit() {
+    this._setting.aplicarTildeByStorage();
   }
 
   cambiarColor(tema: string, link: any) {
     // Una forma de instanciar el DOM es mediante el Inject(Document)
     // y luego hacer referencia a _document en este caso
 
-    const url = `assets/css/colors/${ tema }.css`;
-
-    this._document.getElementById('tema').setAttribute('href', url );
-
+    this._setting.aplicarTema(tema);
     this.aplicarCheck(link);
   }
 
   private aplicarCheck(link: any) {
     // La variable link hace referencia al elemento html seleccionado
 
-    // Obtenemos un Array de elementos por medio de la clase css selector
-    const selector = this._document.getElementsByClassName('selector');
-
-    for (const item of selector) {
-      item.classList.remove('working');
-    }
-
-      link.classList.add('working');
+    this._setting.aplicarTilde(link);
   }
 }
--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -1,8 +1,55 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+import { debug } from 'util';
 
 @Injectable()
 export class SettingsService {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private _document) {
+    const tema = JSON.parse(localStorage.getItem('tema'));
+
+    // Definimos un tema por defecto
+    if (tema == null) {
+      const url = `assets/css/colors/default.css`;
+      this._document.getElementById('tema').setAttribute('href', url );
+    }else {
+      // Usamos el tema del localstorage
+      this.aplicarTema(tema);
+    }
+
+  }
+
+  aplicarTildeByStorage() {
+    // Obtenemos un Array de elementos por medio de la clase css selector
+    const selectores: any = this._document.getElementsByClassName('selector');
+
+    for (const ref of selectores) {
+      if (ref.getAttribute('data-theme') === JSON.parse(localStorage.getItem('tema'))) {
+        ref.classList.add('working');
+      }
+    }
+
+  }
+
+
+  aplicarTema(tema: string) {
+    const url = `assets/css/colors/${ tema }.css`;
+    this._document.getElementById('tema').setAttribute('href', url );
+
+    localStorage.setItem("tema", JSON.stringify(tema));
+
+  }
+
+  aplicarTilde(link: any) {
+    // Obtenemos un Array de elementos por medio de la clase css selector
+    const selector: any = this._document.getElementsByClassName('selector');
+
+    for (const item of selector) {
+      item.classList.remove('working');
+    }
+
+    link.classList.add('working');
+
+  }
 
 }
